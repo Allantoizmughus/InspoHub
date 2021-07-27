c0b6f4e25101f759daccd8d836e122c3251867bf
Allantoizmughus <allan.mwangi@student.moringaschool.com>
2021-07-28 00:51:06 +0300

    Create new class QuoteListActivity

--- app/src/main/java/com/moringaschool/inspohub/MainActivity.java
+++ app/src/main/java/com/moringaschool/inspohub/MainActivity.java
@@ -3,12 +3,23 @@ package com.moringaschool.inspohub;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.AppNameTextView)
+    TextView mAppNameTextView;
+    @BindView(R.id.ViewQuotesButton)
+    Button mViewQuotesButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
     }
 }
---
+++ app/src/main/java/com/moringaschool/inspohub/UI/QuotesListActivity.java
@@ -0,0 +1,16 @@
+package com.moringaschool.inspohub.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.moringaschool.inspohub.R;
+
+public class QuotesListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quotes_list);
+    }
+}
