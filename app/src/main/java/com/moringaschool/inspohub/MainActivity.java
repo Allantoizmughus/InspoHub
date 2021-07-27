8f1ac7e465002c8088a5fc0484a4e4e1c385ad9a
Allantoizmughus <allan.mwangi@student.moringaschool.com>
2021-07-28 01:00:36 +0300

    Implement the main Activity class code

--- app/src/main/java/com/moringaschool/inspohub/MainActivity.java
+++ app/src/main/java/com/moringaschool/inspohub/MainActivity.java
@@ -2,14 +2,18 @@ package com.moringaschool.inspohub;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.moringaschool.inspohub.UI.QuotesListActivity;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     @BindView(R.id.AppNameTextView)
     TextView mAppNameTextView;
     @BindView(R.id.ViewQuotesButton)
@@ -20,6 +24,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        mViewQuotesButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == mViewQuotesButton ){
+           Intent intent = new Intent(MainActivity.this, QuotesListActivity.class);
+           startActivity(intent);
+        }
+
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
 
     }
 }
