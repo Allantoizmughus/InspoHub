f20e560be8b7c62afa4f2c2c1310d99bbc381dda
Allantoizmughus <allan.mwangi@student.moringaschool.com>
2021-07-29 17:43:24 +0300

    Add a click event to navigate from login page to create account page

--- app/src/main/java/com/moringaschool/inspohub/UI/CreateAccountActivity.java
+++ app/src/main/java/com/moringaschool/inspohub/UI/CreateAccountActivity.java
@@ -3,14 +3,44 @@ package com.moringaschool.inspohub.UI;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.moringaschool.inspohub.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class CreateAccountActivity extends AppCompatActivity {
+    @BindView(R.id.nameEditText)
+    EditText mNameEditText;
+    @BindView(R.id.emailEditText) EditText mEmailEditText;
+    @BindView(R.id.passwordEditText)
+    EditText mPasswordEditText;
+    @BindView(R.id.confirmPasswordEditText) EditText mConfirmPasswordEditText;
+    @BindView(R.id.createUserButton)
+    Button mCreateUserButton;
+    @BindView(R.id.loginTextView)
+    TextView mLoginTextView;
+    @BindView(R.id.firebaseProgressBar)
+    ProgressBar mSignInProgressBar;
+    @BindView(R.id.loadingTextView) TextView mLoadingSignUp;
+    private String mName;
+
+
+    public static final String TAG = CreateAccountActivity.class.getSimpleName();
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+        ButterKnife.bind(this);
+
+        m
     }
 }
--- app/src/main/java/com/moringaschool/inspohub/UI/LoginActivity.java
+++ app/src/main/java/com/moringaschool/inspohub/UI/LoginActivity.java
@@ -2,15 +2,52 @@ package com.moringaschool.inspohub.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.moringaschool.inspohub.R;
 
-public class LoginActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.signUpTextView)
+    TextView mSignUpTextView;
+    @BindView(R.id.emailEditText)
+    EditText mEmailEditText;
+    @BindView(R.id.passwordEditText) EditText mPasswordEditText;
+    @BindView(R.id.loginButton)
+    Button mLoginButton;
+    @BindView(R.id.firebaseProgressBar)
+    ProgressBar mSignInProgressBar;
+    @BindView(R.id.loadingTextView) TextView mLoadingSignUp;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+
+        mSignUpTextView.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == mSignUpTextView){
+            Intent intent = new Intent(LoginActivity.this, CreateAccountActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
